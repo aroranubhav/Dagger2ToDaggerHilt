726163e5e3bb5bcd159303c8c7515e463b05beed
maxi <arora.anubhav93@gmail.com>
2025-03-21 22:21:53 +0530

    Initialise app level DI component

--- app/src/main/java/com/almax/dagger2todaggerhilt/GifApplication.kt
+++ app/src/main/java/com/almax/dagger2todaggerhilt/GifApplication.kt
@@ -1,6 +1,24 @@
 package com.almax.dagger2todaggerhilt
 
 import android.app.Application
+import com.almax.dagger2todaggerhilt.di.component.ApplicationComponent
+import com.almax.dagger2todaggerhilt.di.component.DaggerApplicationComponent
+import com.almax.dagger2todaggerhilt.di.module.ApplicationModule
 
-class GifApplication: Application() {
+class GifApplication : Application() {
+
+    lateinit var component: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        injectDependencies()
+    }
+
+    private fun injectDependencies() {
+        component = DaggerApplicationComponent
+            .builder()
+            .applicationModule(ApplicationModule(this@GifApplication))
+            .build()
+        component.inject(this@GifApplication)
+    }
 }
