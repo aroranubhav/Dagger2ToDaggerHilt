19501a6ef532cd30e3aa7a5cf105cda2adf9629a
maxi <arora.anubhav93@gmail.com>
2025-03-22 10:11:36 +0530

    Remove custom qualifiers and scopes

--- app/src/main/java/com/almax/dagger2todaggerhilt/di/qualifiers.kt
+++ app/src/main/java/com/almax/dagger2todaggerhilt/di/qualifiers.kt
@@ -5,14 +5,4 @@ import javax.inject.Qualifier
 @Qualifier
 @MustBeDocumented
 @Retention(AnnotationRetention.BINARY)
-annotation class ApplicationContext
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.BINARY)
 annotation class BaseUrl
-
-@Qualifier
-@MustBeDocumented
-@Retention(AnnotationRetention.BINARY)
-annotation class ActivityContext
