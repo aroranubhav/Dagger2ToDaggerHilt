9d9d4358caf519016fe72d42bbe2c2cc979b5498
maxi <arora.anubhav93@gmail.com>
2025-03-22 10:10:43 +0530

    Update application class
    Add Hilt app annotation as entry point and remove component object creation which was needed for dagger

--- app/src/main/java/com/almax/dagger2todaggerhilt/GifApplication.kt
+++ app/src/main/java/com/almax/dagger2todaggerhilt/GifApplication.kt
@@ -1,24 +1,7 @@
 package com.almax.dagger2todaggerhilt
 
 import android.app.Application
-import com.almax.dagger2todaggerhilt.di.component.ApplicationComponent
-import com.almax.dagger2todaggerhilt.di.component.DaggerApplicationComponent
-import com.almax.dagger2todaggerhilt.di.module.ApplicationModule
+import dagger.hilt.android.HiltAndroidApp
 
-class GifApplication : Application() {
-
-    lateinit var component: ApplicationComponent
-
-    override fun onCreate() {
-        super.onCreate()
-        injectDependencies()
-    }
-
-    private fun injectDependencies() {
-        component = DaggerApplicationComponent
-            .builder()
-            .applicationModule(ApplicationModule(this@GifApplication))
-            .build()
-        component.inject(this@GifApplication)
-    }
-}
+@HiltAndroidApp
+class GifApplication : Application()
